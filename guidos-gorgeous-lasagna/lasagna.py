320c977ff7b59023f306f45c8c71f2b2f17c57be
Alesei N <github.com@bzik.net>
2023-11-30 12:22:17 -0600

    converting indentations spaces to tabs

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -11,21 +11,21 @@ EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
 def bake_time_remaining(elapsed_bake_time):
-    """Calculate the bake time remaining.
+	"""Calculate the bake time remaining.
 
-    :param elapsed_bake_time: int - baking time already elapsed.
-    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+	:param elapsed_bake_time: int - baking time already elapsed.
+	:return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
 
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
-    """
-    return EXPECTED_BAKE_TIME - elapsed_bake_time
+	Function that takes the actual minutes the lasagna has been in the oven as
+	an argument and returns how many minutes the lasagna still needs to bake
+	based on the `EXPECTED_BAKE_TIME`.
+	"""
+	return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 def preparation_time_in_minutes(layers):
-    """ Return preparation time in minutes """
-    return PREPARATION_TIME * layers
+	""" Return preparation time in minutes """
+	return PREPARATION_TIME * layers
 
 def elapsed_time_in_minutes(layers, time):
-    """ Return elapsed time in minutes """
-    return preparation_time_in_minutes(layers) + time
+	""" Return elapsed time in minutes """
+	return preparation_time_in_minutes(layers) + time
